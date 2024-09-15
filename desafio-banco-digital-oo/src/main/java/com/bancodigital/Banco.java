1d69f736361946c4fc07f726ea6fdac924e87f6d
Fábio Signorini <95833161+FabioSigF@users.noreply.github.com>
2024-09-15 12:39:38 -0300

    Desafio de POO Banco Digital

---
+++ desafio-banco-digital-oo/src/main/java/com/bancodigital/Banco.java
@@ -0,0 +1,29 @@
+package com.bancodigital;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Banco {
+
+    @NonNull private String nome;
+    private List<Conta> contas;
+
+    public void listarClientes() {
+        if (contas == null || contas.isEmpty()) {
+            System.out.println("Nenhum cliente cadastrado.");
+            return;
+        }
+
+        System.out.println("Clientes cadastrados no banco " + nome + ":");
+        for (Conta conta : contas) {
+            System.out.println("Cliente: " + conta.getCliente().getNome());
+        }
+    }
+}
---
+++ desafio-banco-digital-oo/src/main/java/com/bancodigital/Main.java
@@ -0,0 +1,24 @@
+package com.bancodigital;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Banco banco = new Banco("Monopoly Banking");
+
+        Cliente fabio = new Cliente();
+        fabio.setNome("Fábio");
+
+        Conta cc = new ContaCorrente(fabio);
+        Conta poupanca = new ContaPoupanca(fabio);
+
+        List<Conta> listaDeContas = new ArrayList<>();
+        listaDeContas.add(cc);
+        listaDeContas.add(poupanca);
+
+        banco.setContas(listaDeContas);
+        banco.listarClientes();
+    }
+}
