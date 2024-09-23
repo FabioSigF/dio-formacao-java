0357a8f90aef0c7d8309f3e8386132e25a23fea2
Fábio Signorini <95833161+FabioSigF@users.noreply.github.com>
2024-09-23 13:39:32 -0300

    Delete fabricadecarros directory

--- fabricadecarros/src/main/java/com/fabricadecarros/fabricadecarros/controller/CarController.java
+++
@@ -1,5 +0,0 @@
-package com.fabricadecarros.fabricadecarros.controller;
-
-public class CarController {
-
-}
--- fabricadecarros/src/main/java/com/fabricadecarros/fabricadecarros/model/Builder.java
+++
@@ -1,9 +0,0 @@
-package com.fabricadecarros.fabricadecarros.model;
-
-public interface Builder {
-    void reset();
-    void setAssentos(int assentos);
-    void setMotor(String motor);
-    void setComputadorBordo(boolean computadorBordo);
-    void setGps(boolean gps);
-}
